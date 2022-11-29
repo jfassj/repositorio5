3469dd71677927c3e09631be894884b64c96b31e
Portilla36 <116852051+Portila36@users.noreply.github.com>
2022-11-29 10:11:07 -0600

    Create index.php

--- index.php
+++ index.php
@@ -14,11 +14,6 @@
     <h5>Estoy en la misma carpeta pero más adentro</h5>
     <h6>qué tan adentro? 😳</h6>
     <h6>Muy adentro🥵🥵🥵</h6>
+    <h6>hola</h6>
 </body>
 </html>
-
-
-
- hOla somos el equip0 1 y hoy v4m0s a hablar de la intrg4ad0ra que nos exigen como si fuera un trabajo para qye al ultimo ni si quiera la vayan a utilizar en esa secundaria debido
- a la complejidad que el "cliente" le esta poniendo. agregando muchas cosas innecesarias y complejas, 
- nadie va a usar el sistema "Gestor de boletas por mas bien que este hecho.
